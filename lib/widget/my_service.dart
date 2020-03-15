fc7e40c9e8bdb89b503a4b0e3fd416a393fdad7c
masterUNG <phrombutr@gmail.com>
2020-03-15 15:54:28 +0700

    finish 15March

--- lib/widget/my_service.dart
+++ lib/widget/my_service.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:ungconstructor14march/utility/my_constant.dart';
 import 'package:ungconstructor14march/widget/authen.dart';
 
 class MyService extends StatefulWidget {
@@ -9,27 +11,84 @@ class MyService extends StatefulWidget {
 
 class _MyServiceState extends State<MyService> {
   // Field
+  String uidString, urlLogin, nameLogin, levelLogin;
 
   // Method
+  @override
+  void initState() {
+    super.initState();
+    findUidFromAuthen();
+  }
+
+  Future<void> findUidFromAuthen() async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    await auth.currentUser().then((value) {
+      uidString = value.uid;
+      print('uid = $uidString');
+      readUserFromFireStore();
+    });
+  }
+
+  Future<void> readUserFromFireStore() async {
+    Firestore firestore = Firestore.instance;
+    CollectionReference collectionReference = firestore.collection('User');
+    await collectionReference.document(uidString).snapshots().listen((event) {
+      print('event ===>> ${event.data}');
+      setState(() {
+        urlLogin = event.data['UrlAvatar'];
+        nameLogin = event.data['Name'];
+        levelLogin = event.data['Level'];
+      });
+    });
+  }
+
   Widget menuSignOut() {
-    return ListTile(onTap: () => processSignOut(),
+    return ListTile(
+      onTap: () => processSignOut(),
       title: Text('Sign Out'),
       leading: Icon(Icons.exit_to_app),
     );
   }
 
-  Future<void> processSignOut()async{
+  Future<void> processSignOut() async {
     FirebaseAuth auth = FirebaseAuth.instance;
     await auth.signOut().then((value) {
-      MaterialPageRoute route = MaterialPageRoute(builder: (value)=>Authen());
+      MaterialPageRoute route = MaterialPageRoute(builder: (value) => Authen());
       Navigator.of(context).pushAndRemoveUntil(route, (route) => false);
     });
+  }
+
+  Widget showAvatar() {
+    return CircleAvatar(
+      backgroundImage: urlLogin == null
+          ? NetworkImage(MyConstant().urlNoAvatar)
+          : NetworkImage(urlLogin),
+    );
+  }
+
+  Widget showHead() {
+    return UserAccountsDrawerHeader(
+      currentAccountPicture: showAvatar(),
+      accountName: showName(),
+      accountEmail: showLevel(),
+    );
+  }
+
+  Text showName() {
+    return nameLogin == null ? Text('Name') : Text(nameLogin);
+  }
+
+  Text showLevel() {
+    return levelLogin == null ? Text('Level') : Text(levelLogin);
   }
 
   Widget showDrawer() {
     return Drawer(
       child: ListView(
-        children: <Widget>[menuSignOut(),],
+        children: <Widget>[
+          showHead(),
+          menuSignOut(),
+        ],
       ),
     );
   }
